efb6640745d74fa458b9a33fb96b49bb07a6511a
Jon Greazel <jdgreazel@gmail.com>
2024-09-20 17:27:26 -0500

    add links

--- Sandwich/SandwichComponent/SandwichServiceExtensions.cs
+++ Sandwich/SandwichComponent/SandwichServiceExtensions.cs
@@ -12,6 +12,9 @@ namespace SandwichComponent
             // add service
             // then utilize inside of api project
             // fix double json file thing (proj and sol)
+            // https://chatgpt.com/c/66eb9fd3-546c-800d-8d59-ecdee2f37925
+            // https://github.com/danielgerlag/workflow-core/blob/master/src/samples/WorkflowCore.Sample01/Program.cs
+            // https://workflow-core.readthedocs.io/en/latest/getting-started/ 
             return services;
         }
     }
