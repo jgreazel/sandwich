c484fa6a1a433b057ee83f41e0bd29d191c1adea
Jon Greazel <jdgreazel@gmail.com>
2024-09-20 17:26:17 -0500

    add todos

--- Sandwich/SandwichComponent/SandwichServiceExtensions.cs
+++ Sandwich/SandwichComponent/SandwichServiceExtensions.cs
@@ -10,6 +10,8 @@ namespace SandwichComponent
             // configure wf repo from config
             // add workflow core deps
             // add service
+            // then utilize inside of api project
+            // fix double json file thing (proj and sol)
             return services;
         }
     }
